fc9e03ff01554a2d7c6e1c76c517e3080e55a531
Xiaotiantian001 <75616739+Xiaotiantian001@users.noreply.github.com>
2021-01-23 16:17:50 +0800

    Update CentOS6.sh

--- CentOS6.sh
+++ CentOS6.sh
@@ -1,2 +1,2 @@
-rm -rf /etc/yum.repos.d/CentOS* && wget -O /etc/yum.repos.d/CentOS-Base.repo https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/CentOS6-Base.repo
-rm -rf /home/Centos6.5_64.tar.gz && wget -O /home/Centos6.5_64.tar.gz https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/Centos6.5_64.tar.gz
+rm -rf /etc/yum.repos.d/CentOS* && wget --no-check-certificate -O /etc/yum.repos.d/CentOS-Base.repo https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/CentOS6-Base.repo
+rm -rf /home/Centos6.5_64.tar.gz && wget --no-check-certificate -O /home/Centos6.5_64.tar.gz https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/Centos6.5_64.tar.gz
